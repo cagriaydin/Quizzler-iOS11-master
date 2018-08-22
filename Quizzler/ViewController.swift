f27100b38b58617e5cce24cfdb2b6f3d3c4c6b5d
Çağrı Aydın <c.aydinakdogan@gmail.com>
2018-08-22 18:22:00 +0300

    quiz flow implemented and alert added

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +22,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
     }
     
     
@@ -35,17 +44,35 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
-        
+        questionLabel.text = allQuestions.list[questionNumber].questionText
     }
     
     
     func checkAnswer() {
-        
+        if(allQuestions.list[questionNumber].answer == pickedAnswer){
+            
+            if(questionNumber < allQuestions.list.count - 1){
+                questionNumber += 1
+                nextQuestion()
+            }
+            else{
+                let alert = UIAlertController(title: "Congratulations !", message: "You've finished all the questions, do you want to start over ?", preferredStyle: .alert)
+                
+                let restartAction = UIAlertAction(title: "Restart", style: .default) { (action) in
+                    self.startOver()
+                }
+                
+                alert.addAction(restartAction)
+                present(alert, animated: true, completion: nil)
+            }
+            
+        }
     }
     
     
     func startOver() {
-       
+        questionNumber = 0
+        nextQuestion()
     }
     
 
